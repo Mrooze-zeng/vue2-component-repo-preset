b5ee8545b8b43520ba8e87f36488dc189b3909d6
John <mrooze@v-hiker.cn>
2021-09-10 15:44:24 +0800

    添加更新preset命令

--- generator/template/vue-service/updateTemplate.js
+++ generator/template/vue-service/updateTemplate.js
@@ -1,36 +1,35 @@
 // @ts-ignore
 // prettier-ignore
 const tpls = <%- JSON.stringify(tpls)%>;
-
+const projectName ="<%= projectName %>";
 
 const presetDir = "vue-cli-presets/vue2-component-repo-preset.git/generator";
 
 const path = require("path");
-const os = require("os")
+const os = require("os");
 const fs = require("fs");
 const ejs = require("ejs");
 const { warn } = require("@vue/cli-shared-utils/lib/logger");
 
-const tplDir = path.resolve(os.tmpdir(),presetDir)
-
+const tplDir = path.resolve(os.tmpdir(), presetDir);
 
 const remotePreset =
   "direct:https://github.com/Mrooze-zeng/vue2-component-repo-preset.git";
 
 const loadRemotePreset = require("@vue/cli/lib/util/loadRemotePreset");
 
-const replaceFile = function (tpls = {}, currentFile = "", target = "") {
+const replaceFile = function (tpls = {},  target = "") {
   for (const file in tpls) {
-    const buf = fs.readFileSync(path.resolve(target, tpls[file]));
-    if (file != currentFile) {
-      fs.writeFileSync(path.resolve(__dirname, "../", file), buf);
-    } else {
-      fs.writeFileSync(
-        path.resolve(__dirname, "../", file),
-        ejs.render(buf.toString(), { tpls }),
-      );
+    if(file.startsWith(".")){
+      continue
     }
-    warn(`替换${file}`)
+    const buf = fs.readFileSync(path.resolve(target, tpls[file]));
+    fs.writeFileSync(
+      path.resolve(__dirname, "../", file),
+      ejs.render(buf.toString(), { tpls ,projectName}),
+    );
+
+    warn(`替换 ${file}`);
   }
 };
 
@@ -41,13 +40,8 @@ module.exports = {
     usage: "vue-cli-service updateTpl",
   },
   action: async function () {
-    const isExist = fs.lstatSync(tplDir);
-    const currentFile = path.join(
-      path.basename(__dirname),
-      path.basename(__filename),
-    );
-    if (isExist) {
-      replaceFile(tpls, currentFile, tplDir);
+    if (fs.lstatSync(tplDir)) {
+      replaceFile(tpls, tplDir);
     } else {
       let presetDir;
       const { plugins } = await loadRemotePreset(remotePreset);
@@ -59,7 +53,7 @@ module.exports = {
           presetDir = path.resolve(plugin, "generator");
         }
       }
-      replaceFile(tpls, currentFile, presetDir);
+      replaceFile(tpls, presetDir);
     }
   },
 };
