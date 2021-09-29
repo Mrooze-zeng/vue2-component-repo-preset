e16631521aba8184bc5ba90cdf95b3ec0090cedf
John <mrooze@v-hiker.cn>
2021-09-29 19:42:24 +0800

    防止配置误更改

--- generator/template/vue.config.js
+++ generator/template/vue.config.js
@@ -1,3 +1,9 @@
+/*
+ * 永远不要尝试修改这个文件，
+ * 因为这个文件永远与git@github.com:Mrooze-zeng/vue2-component-repo-preset.git保持一致!!!!
+ * 有新需求请提Issue.
+ */
+
 const webpack = require("webpack");
 
 module.exports = {
